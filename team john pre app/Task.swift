6b9c0810a73d62b0d18b3b7ba897ac6f36766a7b
juliev42 <juliev@mit.edu>
2017-03-04 13:26:13 -0500

    Add files via upload
    prototype of the application with just initial task list board

---
+++ team john pre app/Task.swift
@@ -0,0 +1,17 @@
+//
+//  Task.swift
+//  team john pre app
+//
+//  Created by Julie Vaughn on 3/4/17.
+//  Copyright © 2017 Julie Vaughn. All rights reserved.
+//
+
+import Foundation
+
+struct Task{
+    var name: String?
+    
+    init(name: String?){
+        self.name = name
+    }
+}
---
+++ team john pre app/TaskNames.swift
@@ -0,0 +1,16 @@
+//
+//  TaskNames.swift
+//  team john pre app
+//
+//  Created by Julie Vaughn on 3/4/17.
+//  Copyright © 2017 Julie Vaughn. All rights reserved.
+//
+
+import Foundation
+
+let taskData = [
+    Task(name: "Set the Table"),
+    Task(name: "Make the Bed"),
+    Task(name: "Take out the Trash")
+]
+
